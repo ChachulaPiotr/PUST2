887c24d45b5e41f7e8a9d040f350c25e63439088
Piotr <pchachula@gmail.com>
2019-04-04 18:39:49 +0200

    bla

--- Proj/Zad1.m
+++ Proj/Zad1.m
@@ -1,24 +1,25 @@
-k = 1000;
+k = 200;
 u = zeros(k,1);
 y = u;
 z = u;
 Y = zeros(1000);
+a = 2;
 u(k) = 0;
-z(k) = 1;
+z(k) = a;
 for i = 1:k
     y = circshift(y,-1);
     y(k)=symulacja_obiektu4y(u(k-6),u(k-7),z(k-2),z(k-3),y(k-1),y(k-2));
     u = circshift(u,-1);
     z = circshift(z,-1);
     u(k) = 0;
-    z(k) = 1;
+    z(k) = a;
 end
-plot(y);
-hold on;
+% plot(y);
+% hold on;
 plot(z);
-hold off;
+hold on;
 xlabel('k');
-ylabel('y(k), z(k)');
-legend('y','z');
-saveas(gcf,'odp_skok_z.eps','epsc');
+ylabel('z(k)');
+% legend('y','u');
+% saveas(gcf,'odp_skok_z.eps','epsc');
 %save('odp_skok_u.mat','y');
